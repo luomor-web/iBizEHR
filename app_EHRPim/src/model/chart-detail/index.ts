54bc1b1ffde02c679a46f7871cf5d7563ddcfe89
ibizdev <ibizdev>
2020-06-08 13:06:49 +0800

    jackwang 部署微服务应用

--- app_EHRPim/src/model/chart-detail/index.ts
+++
@@ -1,6 +0,0 @@
-export { ChartDataSetField } from './chart-dataset-field';
-export { ChartLineSeries } from './chart-line-series';
-export { ChartFunnelSeries } from './chart-funnel-series';
-export { ChartPieSeries } from './chart-pie-series';
-export { ChartBarSeries } from './chart-bar-series';
-export { ChartRadarSeries } from './chart-radar-series';
--- ehr-provider/ehr-provider-webapi/src/main/java/cn/ibizlab/ehr/webapi/config/WebApiRestConfiguration.java
+++
@@ -1,8 +0,0 @@
-package cn.ibizlab.ehr.webapi.config;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan("cn.ibizlab.ehr.webapi")
-public class WebApiRestConfiguration {
-
-}
