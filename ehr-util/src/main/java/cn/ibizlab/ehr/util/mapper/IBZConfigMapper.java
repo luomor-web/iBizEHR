75cf4e54b2bb47917d8ee621885621ed4924e117
ibizdev <ibizdev>
2020-06-08 13:38:40 +0800

    jackwang 部署微服务应用

--- ehr-util/src/main/java/cn/ibizlab/ehr/util/mapper/IBZConfigMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.ibizlab.ehr.util.mapper;
-
-import cn.ibizlab.ehr.util.domain.IBZConfig;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface IBZConfigMapper extends BaseMapper<IBZConfig>{
-
-}
