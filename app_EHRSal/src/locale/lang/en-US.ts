7dc037bc3e58efed83c6f90689045beed80505c2
ibizdev <ibizdev>
2020-06-11 16:22:32 +0800

    jackwang 部署微服务应用

--- app_EHRSal/src/locale/lang/en-US.ts
+++ app_EHRSal/src/locale/lang/en-US.ts
@@ -1,29 +1,29 @@
 import salscheme_en_US from '@locale/lanres/entities/sal-scheme/sal-scheme_en_US';
-import salruledetail_en_US from '@locale/lanres/entities/sal-rule-detail/sal-rule-detail_en_US';
-import salrule_en_US from '@locale/lanres/entities/sal-rule/sal-rule_en_US';
-import salsource_en_US from '@locale/lanres/entities/sal-source/sal-source_en_US';
-import salparam_en_US from '@locale/lanres/entities/sal-param/sal-param_en_US';
-import ormorg_en_US from '@locale/lanres/entities/orm-org/orm-org_en_US';
-import salsubject_en_US from '@locale/lanres/entities/sal-subject/sal-subject_en_US';
-import saltype_en_US from '@locale/lanres/entities/sal-type/sal-type_en_US';
-import salitem_en_US from '@locale/lanres/entities/sal-item/sal-item_en_US';
-import salschemeitem_en_US from '@locale/lanres/entities/sal-scheme-item/sal-scheme-item_en_US';
 import codelist1_en_US from '@locale/lanres/entities/code-list1/code-list1_en_US';
+import salruledetail_en_US from '@locale/lanres/entities/sal-rule-detail/sal-rule-detail_en_US';
 import sallog_en_US from '@locale/lanres/entities/sal-log/sal-log_en_US';
 import salstdzyzg_en_US from '@locale/lanres/entities/sal-stdzyzg/sal-stdzyzg_en_US';
+import salrule_en_US from '@locale/lanres/entities/sal-rule/sal-rule_en_US';
 import salpersonstddetail_en_US from '@locale/lanres/entities/salpersonstddetail/salpersonstddetail_en_US';
+import salsource_en_US from '@locale/lanres/entities/sal-source/sal-source_en_US';
 import salstdgw_en_US from '@locale/lanres/entities/sal-stdgw/sal-stdgw_en_US';
 import salstdgwgz_en_US from '@locale/lanres/entities/sal-stdgwgz/sal-stdgwgz_en_US';
+import salparam_en_US from '@locale/lanres/entities/sal-param/sal-param_en_US';
 import salstdxmgz_en_US from '@locale/lanres/entities/sal-std-xmgz/sal-std-xmgz_en_US';
 import pimperson_en_US from '@locale/lanres/entities/pim-person/pim-person_en_US';
 import ormpost_en_US from '@locale/lanres/entities/orm-post/orm-post_en_US';
+import ormorg_en_US from '@locale/lanres/entities/orm-org/orm-org_en_US';
+import salsubject_en_US from '@locale/lanres/entities/sal-subject/sal-subject_en_US';
 import salstdzc_en_US from '@locale/lanres/entities/sal-stdzc/sal-stdzc_en_US';
 import salsalarybill_en_US from '@locale/lanres/entities/sal-salary-bill/sal-salary-bill_en_US';
+import saltype_en_US from '@locale/lanres/entities/sal-type/sal-type_en_US';
 import salsalary_en_US from '@locale/lanres/entities/sal-salary/sal-salary_en_US';
+import salitem_en_US from '@locale/lanres/entities/sal-item/sal-item_en_US';
 import ormqygl_en_US from '@locale/lanres/entities/orm-qygl/orm-qygl_en_US';
 import ormduty_en_US from '@locale/lanres/entities/orm-duty/orm-duty_en_US';
 import codeitem_en_US from '@locale/lanres/entities/code-item/code-item_en_US';
 import salsalarydetail_en_US from '@locale/lanres/entities/sal-salary-detail/sal-salary-detail_en_US';
+import salschemeitem_en_US from '@locale/lanres/entities/sal-scheme-item/sal-scheme-item_en_US';
 import salplan_en_US from '@locale/lanres/entities/sal-plan/sal-plan_en_US';
 import ormorgsector_en_US from '@locale/lanres/entities/orm-orgsector/orm-orgsector_en_US';
 import salstdzx_en_US from '@locale/lanres/entities/sal-stdzx/sal-stdzx_en_US';
@@ -115,31 +115,31 @@ export default {
     },
     entities: {
         salscheme: salscheme_en_US,
-        salruledetail: salruledetail_en_US,
-        salrule: salrule_en_US,
-        salsource: salsource_en_US,
-        salparam: salparam_en_US,
-        ormorg: ormorg_en_US,
-        salsubject: salsubject_en_US,
-        saltype: saltype_en_US,
-        salitem: salitem_en_US,
-        salschemeitem: salschemeitem_en_US,
         codelist1: codelist1_en_US,
+        salruledetail: salruledetail_en_US,
         sallog: sallog_en_US,
         salstdzyzg: salstdzyzg_en_US,
+        salrule: salrule_en_US,
         salpersonstddetail: salpersonstddetail_en_US,
+        salsource: salsource_en_US,
         salstdgw: salstdgw_en_US,
         salstdgwgz: salstdgwgz_en_US,
+        salparam: salparam_en_US,
         salstdxmgz: salstdxmgz_en_US,
         pimperson: pimperson_en_US,
         ormpost: ormpost_en_US,
+        ormorg: ormorg_en_US,
+        salsubject: salsubject_en_US,
         salstdzc: salstdzc_en_US,
         salsalarybill: salsalarybill_en_US,
+        saltype: saltype_en_US,
         salsalary: salsalary_en_US,
+        salitem: salitem_en_US,
         ormqygl: ormqygl_en_US,
         ormduty: ormduty_en_US,
         codeitem: codeitem_en_US,
         salsalarydetail: salsalarydetail_en_US,
+        salschemeitem: salschemeitem_en_US,
         salplan: salplan_en_US,
         ormorgsector: ormorgsector_en_US,
         salstdzx: salstdzx_en_US,
--- app_EHRSal/src/locale/lang/zh-CN.ts
+++ app_EHRSal/src/locale/lang/zh-CN.ts
@@ -1,29 +1,29 @@
 import salscheme_zh_CN from '@locale/lanres/entities/sal-scheme/sal-scheme_zh_CN';
-import salruledetail_zh_CN from '@locale/lanres/entities/sal-rule-detail/sal-rule-detail_zh_CN';
-import salrule_zh_CN from '@locale/lanres/entities/sal-rule/sal-rule_zh_CN';
-import salsource_zh_CN from '@locale/lanres/entities/sal-source/sal-source_zh_CN';
-import salparam_zh_CN from '@locale/lanres/entities/sal-param/sal-param_zh_CN';
-import ormorg_zh_CN from '@locale/lanres/entities/orm-org/orm-org_zh_CN';
-import salsubject_zh_CN from '@locale/lanres/entities/sal-subject/sal-subject_zh_CN';
-import saltype_zh_CN from '@locale/lanres/entities/sal-type/sal-type_zh_CN';
-import salitem_zh_CN from '@locale/lanres/entities/sal-item/sal-item_zh_CN';
-import salschemeitem_zh_CN from '@locale/lanres/entities/sal-scheme-item/sal-scheme-item_zh_CN';
 import codelist1_zh_CN from '@locale/lanres/entities/code-list1/code-list1_zh_CN';
+import salruledetail_zh_CN from '@locale/lanres/entities/sal-rule-detail/sal-rule-detail_zh_CN';
 import sallog_zh_CN from '@locale/lanres/entities/sal-log/sal-log_zh_CN';
 import salstdzyzg_zh_CN from '@locale/lanres/entities/sal-stdzyzg/sal-stdzyzg_zh_CN';
+import salrule_zh_CN from '@locale/lanres/entities/sal-rule/sal-rule_zh_CN';
 import salpersonstddetail_zh_CN from '@locale/lanres/entities/salpersonstddetail/salpersonstddetail_zh_CN';
+import salsource_zh_CN from '@locale/lanres/entities/sal-source/sal-source_zh_CN';
 import salstdgw_zh_CN from '@locale/lanres/entities/sal-stdgw/sal-stdgw_zh_CN';
 import salstdgwgz_zh_CN from '@locale/lanres/entities/sal-stdgwgz/sal-stdgwgz_zh_CN';
+import salparam_zh_CN from '@locale/lanres/entities/sal-param/sal-param_zh_CN';
 import salstdxmgz_zh_CN from '@locale/lanres/entities/sal-std-xmgz/sal-std-xmgz_zh_CN';
 import pimperson_zh_CN from '@locale/lanres/entities/pim-person/pim-person_zh_CN';
 import ormpost_zh_CN from '@locale/lanres/entities/orm-post/orm-post_zh_CN';
+import ormorg_zh_CN from '@locale/lanres/entities/orm-org/orm-org_zh_CN';
+import salsubject_zh_CN from '@locale/lanres/entities/sal-subject/sal-subject_zh_CN';
 import salstdzc_zh_CN from '@locale/lanres/entities/sal-stdzc/sal-stdzc_zh_CN';
 import salsalarybill_zh_CN from '@locale/lanres/entities/sal-salary-bill/sal-salary-bill_zh_CN';
+import saltype_zh_CN from '@locale/lanres/entities/sal-type/sal-type_zh_CN';
 import salsalary_zh_CN from '@locale/lanres/entities/sal-salary/sal-salary_zh_CN';
+import salitem_zh_CN from '@locale/lanres/entities/sal-item/sal-item_zh_CN';
 import ormqygl_zh_CN from '@locale/lanres/entities/orm-qygl/orm-qygl_zh_CN';
 import ormduty_zh_CN from '@locale/lanres/entities/orm-duty/orm-duty_zh_CN';
 import codeitem_zh_CN from '@locale/lanres/entities/code-item/code-item_zh_CN';
 import salsalarydetail_zh_CN from '@locale/lanres/entities/sal-salary-detail/sal-salary-detail_zh_CN';
+import salschemeitem_zh_CN from '@locale/lanres/entities/sal-scheme-item/sal-scheme-item_zh_CN';
 import salplan_zh_CN from '@locale/lanres/entities/sal-plan/sal-plan_zh_CN';
 import ormorgsector_zh_CN from '@locale/lanres/entities/orm-orgsector/orm-orgsector_zh_CN';
 import salstdzx_zh_CN from '@locale/lanres/entities/sal-stdzx/sal-stdzx_zh_CN';
@@ -115,31 +115,31 @@ export default {
     },
     entities: {
         salscheme: salscheme_zh_CN,
-        salruledetail: salruledetail_zh_CN,
-        salrule: salrule_zh_CN,
-        salsource: salsource_zh_CN,
-        salparam: salparam_zh_CN,
-        ormorg: ormorg_zh_CN,
-        salsubject: salsubject_zh_CN,
-        saltype: saltype_zh_CN,
-        salitem: salitem_zh_CN,
-        salschemeitem: salschemeitem_zh_CN,
         codelist1: codelist1_zh_CN,
+        salruledetail: salruledetail_zh_CN,
         sallog: sallog_zh_CN,
         salstdzyzg: salstdzyzg_zh_CN,
+        salrule: salrule_zh_CN,
         salpersonstddetail: salpersonstddetail_zh_CN,
+        salsource: salsource_zh_CN,
         salstdgw: salstdgw_zh_CN,
         salstdgwgz: salstdgwgz_zh_CN,
+        salparam: salparam_zh_CN,
         salstdxmgz: salstdxmgz_zh_CN,
         pimperson: pimperson_zh_CN,
         ormpost: ormpost_zh_CN,
+        ormorg: ormorg_zh_CN,
+        salsubject: salsubject_zh_CN,
         salstdzc: salstdzc_zh_CN,
         salsalarybill: salsalarybill_zh_CN,
+        saltype: saltype_zh_CN,
         salsalary: salsalary_zh_CN,
+        salitem: salitem_zh_CN,
         ormqygl: ormqygl_zh_CN,
         ormduty: ormduty_zh_CN,
         codeitem: codeitem_zh_CN,
         salsalarydetail: salsalarydetail_zh_CN,
+        salschemeitem: salschemeitem_zh_CN,
         salplan: salplan_zh_CN,
         ormorgsector: ormorgsector_zh_CN,
         salstdzx: salstdzx_zh_CN,
