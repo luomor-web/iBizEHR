9db19f1048793d8daf3efe08a87ebd270572d5c0
ibizdev <ibizdev>
2020-06-12 11:40:56 +0800

    Miracle 部署微服务接口

--- ehr-provider/ehr-provider-webapi/src/main/java/cn/ibizlab/ehr/webapi/rest/ParIntegralRuleResource.java
+++ ehr-provider/ehr-provider-webapi/src/main/java/cn/ibizlab/ehr/webapi/rest/ParIntegralRuleResource.java
@@ -46,5 +46,117 @@ public class ParIntegralRuleResource {
     @Lazy
     public ParIntegralRuleMapping parintegralruleMapping;
 
+    @ApiOperation(value = "检查积分规则", tags = {"积分规则" },  notes = "检查积分规则")
+	@RequestMapping(method = RequestMethod.POST, value = "/parintegralrules/checkkey")
+    public ResponseEntity<Boolean> checkKey(@RequestBody ParIntegralRuleDTO parintegralruledto) {
+        return  ResponseEntity.status(HttpStatus.OK).body(parintegralruleService.checkKey(parintegralruleMapping.toDomain(parintegralruledto)));
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleService.get(#parintegralrule_id),'ehr-ParIntegralRule-Remove')")
+    @ApiOperation(value = "删除积分规则", tags = {"积分规则" },  notes = "删除积分规则")
+	@RequestMapping(method = RequestMethod.DELETE, value = "/parintegralrules/{parintegralrule_id}")
+    @Transactional
+    public ResponseEntity<Boolean> remove(@PathVariable("parintegralrule_id") String parintegralrule_id) {
+         return ResponseEntity.status(HttpStatus.OK).body(parintegralruleService.remove(parintegralrule_id));
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleService.getParintegralruleByIds(#ids),'ehr-ParIntegralRule-Remove')")
+    @ApiOperation(value = "批量删除积分规则", tags = {"积分规则" },  notes = "批量删除积分规则")
+	@RequestMapping(method = RequestMethod.DELETE, value = "/parintegralrules/batch")
+    public ResponseEntity<Boolean> removeBatch(@RequestBody List<String> ids) {
+        parintegralruleService.removeBatch(ids);
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleService.get(#parintegralrule_id),'ehr-ParIntegralRule-Update')")
+    @ApiOperation(value = "更新积分规则", tags = {"积分规则" },  notes = "更新积分规则")
+	@RequestMapping(method = RequestMethod.PUT, value = "/parintegralrules/{parintegralrule_id}")
+    @Transactional
+    public ResponseEntity<ParIntegralRuleDTO> update(@PathVariable("parintegralrule_id") String parintegralrule_id, @RequestBody ParIntegralRuleDTO parintegralruledto) {
+		ParIntegralRule domain  = parintegralruleMapping.toDomain(parintegralruledto);
+        domain .setParintegralruleid(parintegralrule_id);
+		parintegralruleService.update(domain );
+		ParIntegralRuleDTO dto = parintegralruleMapping.toDto(domain );
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleService.getParintegralruleByEntities(this.parintegralruleMapping.toDomain(#parintegralruledtos)),'ehr-ParIntegralRule-Update')")
+    @ApiOperation(value = "批量更新积分规则", tags = {"积分规则" },  notes = "批量更新积分规则")
+	@RequestMapping(method = RequestMethod.PUT, value = "/parintegralrules/batch")
+    public ResponseEntity<Boolean> updateBatch(@RequestBody List<ParIntegralRuleDTO> parintegralruledtos) {
+        parintegralruleService.updateBatch(parintegralruleMapping.toDomain(parintegralruledtos));
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @PostAuthorize("hasPermission(this.parintegralruleMapping.toDomain(returnObject.body),'ehr-ParIntegralRule-Get')")
+    @ApiOperation(value = "获取积分规则", tags = {"积分规则" },  notes = "获取积分规则")
+	@RequestMapping(method = RequestMethod.GET, value = "/parintegralrules/{parintegralrule_id}")
+    public ResponseEntity<ParIntegralRuleDTO> get(@PathVariable("parintegralrule_id") String parintegralrule_id) {
+        ParIntegralRule domain = parintegralruleService.get(parintegralrule_id);
+        ParIntegralRuleDTO dto = parintegralruleMapping.toDto(domain);
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @ApiOperation(value = "获取积分规则草稿", tags = {"积分规则" },  notes = "获取积分规则草稿")
+	@RequestMapping(method = RequestMethod.GET, value = "/parintegralrules/getdraft")
+    public ResponseEntity<ParIntegralRuleDTO> getDraft() {
+        return ResponseEntity.status(HttpStatus.OK).body(parintegralruleMapping.toDto(parintegralruleService.getDraft(new ParIntegralRule())));
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleMapping.toDomain(#parintegralruledto),'ehr-ParIntegralRule-Create')")
+    @ApiOperation(value = "新建积分规则", tags = {"积分规则" },  notes = "新建积分规则")
+	@RequestMapping(method = RequestMethod.POST, value = "/parintegralrules")
+    @Transactional
+    public ResponseEntity<ParIntegralRuleDTO> create(@RequestBody ParIntegralRuleDTO parintegralruledto) {
+        ParIntegralRule domain = parintegralruleMapping.toDomain(parintegralruledto);
+		parintegralruleService.create(domain);
+        ParIntegralRuleDTO dto = parintegralruleMapping.toDto(domain);
+		return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleMapping.toDomain(#parintegralruledtos),'ehr-ParIntegralRule-Create')")
+    @ApiOperation(value = "批量新建积分规则", tags = {"积分规则" },  notes = "批量新建积分规则")
+	@RequestMapping(method = RequestMethod.POST, value = "/parintegralrules/batch")
+    public ResponseEntity<Boolean> createBatch(@RequestBody List<ParIntegralRuleDTO> parintegralruledtos) {
+        parintegralruleService.createBatch(parintegralruleMapping.toDomain(parintegralruledtos));
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleMapping.toDomain(#parintegralruledto),'ehr-ParIntegralRule-Save')")
+    @ApiOperation(value = "保存积分规则", tags = {"积分规则" },  notes = "保存积分规则")
+	@RequestMapping(method = RequestMethod.POST, value = "/parintegralrules/save")
+    public ResponseEntity<Boolean> save(@RequestBody ParIntegralRuleDTO parintegralruledto) {
+        return ResponseEntity.status(HttpStatus.OK).body(parintegralruleService.save(parintegralruleMapping.toDomain(parintegralruledto)));
+    }
+
+    @PreAuthorize("hasPermission(this.parintegralruleMapping.toDomain(#parintegralruledtos),'ehr-ParIntegralRule-Save')")
+    @ApiOperation(value = "批量保存积分规则", tags = {"积分规则" },  notes = "批量保存积分规则")
+	@RequestMapping(method = RequestMethod.POST, value = "/parintegralrules/savebatch")
+    public ResponseEntity<Boolean> saveBatch(@RequestBody List<ParIntegralRuleDTO> parintegralruledtos) {
+        parintegralruleService.saveBatch(parintegralruleMapping.toDomain(parintegralruledtos));
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_SUPERADMIN','ehr-ParIntegralRule-Default-all')")
+	@ApiOperation(value = "获取DEFAULT", tags = {"积分规则" } ,notes = "获取DEFAULT")
+    @RequestMapping(method= RequestMethod.GET , value="/parintegralrules/fetchdefault")
+	public ResponseEntity<List<ParIntegralRuleDTO>> fetchDefault(ParIntegralRuleSearchContext context) {
+        Page<ParIntegralRule> domains = parintegralruleService.searchDefault(context) ;
+        List<ParIntegralRuleDTO> list = parintegralruleMapping.toDto(domains.getContent());
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("x-page", String.valueOf(context.getPageable().getPageNumber()))
+                .header("x-per-page", String.valueOf(context.getPageable().getPageSize()))
+                .header("x-total", String.valueOf(domains.getTotalElements()))
+                .body(list);
+	}
+
+    @PreAuthorize("hasAnyAuthority('ROLE_SUPERADMIN','ehr-ParIntegralRule-Default-all')")
+	@ApiOperation(value = "查询DEFAULT", tags = {"积分规则" } ,notes = "查询DEFAULT")
+    @RequestMapping(method= RequestMethod.POST , value="/parintegralrules/searchdefault")
+	public ResponseEntity<Page<ParIntegralRuleDTO>> searchDefault(@RequestBody ParIntegralRuleSearchContext context) {
+        Page<ParIntegralRule> domains = parintegralruleService.searchDefault(context) ;
+	    return ResponseEntity.status(HttpStatus.OK)
+                .body(new PageImpl(parintegralruleMapping.toDto(domains.getContent()), context.getPageable(), domains.getTotalElements()));
+	}
 }
 
