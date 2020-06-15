01677a167e5c5d2e49398c22e8e9abb10cce3476
ibizdev <ibizdev>
2020-06-15 16:54:03 +0800

    Miracle 部署微服务接口

--- ehr-provider/ehr-provider-webapi/src/main/java/cn/ibizlab/ehr/webapi/rest/ParKhzcmxResource.java
+++ ehr-provider/ehr-provider-webapi/src/main/java/cn/ibizlab/ehr/webapi/rest/ParKhzcmxResource.java
@@ -159,6 +159,138 @@ public class ParKhzcmxResource {
                 .body(new PageImpl(parkhzcmxMapping.toDto(domains.getContent()), context.getPageable(), domains.getTotalElements()));
 	}
     @PreAuthorize("hasPermission(this.parkhzcmxMapping.toDomain(#parkhzcmxdto),'ehr-ParKhzcmx-Create')")
+    @ApiOperation(value = "根据考核模板建立考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板建立考核模板明细")
+	@RequestMapping(method = RequestMethod.POST, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes")
+    @Transactional
+    public ResponseEntity<ParKhzcmxDTO> createByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @RequestBody ParKhzcmxDTO parkhzcmxdto) {
+        ParKhzcmx domain = parkhzcmxMapping.toDomain(parkhzcmxdto);
+        domain.setParassesstemplateid(parassesstemplate_id);
+		parkhzcmxService.create(domain);
+        ParKhzcmxDTO dto = parkhzcmxMapping.toDto(domain);
+		return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @PreAuthorize("hasPermission(this.parkhzcmxMapping.toDomain(#parkhzcmxdtos),'ehr-ParKhzcmx-Create')")
+    @ApiOperation(value = "根据考核模板批量建立考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板批量建立考核模板明细")
+	@RequestMapping(method = RequestMethod.POST, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/batch")
+    public ResponseEntity<Boolean> createBatchByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @RequestBody List<ParKhzcmxDTO> parkhzcmxdtos) {
+        List<ParKhzcmx> domainlist=parkhzcmxMapping.toDomain(parkhzcmxdtos);
+        for(ParKhzcmx domain:domainlist){
+            domain.setParassesstemplateid(parassesstemplate_id);
+        }
+        parkhzcmxService.createBatch(domainlist);
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @PreAuthorize("hasPermission(this.parkhzcmxMapping.toDomain(#parkhzcmxdto),'ehr-ParKhzcmx-Save')")
+    @ApiOperation(value = "根据考核模板保存考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板保存考核模板明细")
+	@RequestMapping(method = RequestMethod.POST, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/save")
+    public ResponseEntity<Boolean> saveByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @RequestBody ParKhzcmxDTO parkhzcmxdto) {
+        ParKhzcmx domain = parkhzcmxMapping.toDomain(parkhzcmxdto);
+        domain.setParassesstemplateid(parassesstemplate_id);
+        return ResponseEntity.status(HttpStatus.OK).body(parkhzcmxService.save(domain));
+    }
+
+    @PreAuthorize("hasPermission(this.parkhzcmxMapping.toDomain(#parkhzcmxdtos),'ehr-ParKhzcmx-Save')")
+    @ApiOperation(value = "根据考核模板批量保存考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板批量保存考核模板明细")
+	@RequestMapping(method = RequestMethod.POST, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/savebatch")
+    public ResponseEntity<Boolean> saveBatchByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @RequestBody List<ParKhzcmxDTO> parkhzcmxdtos) {
+        List<ParKhzcmx> domainlist=parkhzcmxMapping.toDomain(parkhzcmxdtos);
+        for(ParKhzcmx domain:domainlist){
+             domain.setParassesstemplateid(parassesstemplate_id);
+        }
+        parkhzcmxService.saveBatch(domainlist);
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @PostAuthorize("hasPermission(this.parkhzcmxMapping.toDomain(returnObject.body),'ehr-ParKhzcmx-Get')")
+    @ApiOperation(value = "根据考核模板获取考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板获取考核模板明细")
+	@RequestMapping(method = RequestMethod.GET, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/{parkhzcmx_id}")
+    public ResponseEntity<ParKhzcmxDTO> getByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @PathVariable("parkhzcmx_id") String parkhzcmx_id) {
+        ParKhzcmx domain = parkhzcmxService.get(parkhzcmx_id);
+        ParKhzcmxDTO dto = parkhzcmxMapping.toDto(domain);
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @ApiOperation(value = "根据考核模板获取考核模板明细草稿", tags = {"考核模板明细" },  notes = "根据考核模板获取考核模板明细草稿")
+    @RequestMapping(method = RequestMethod.GET, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/getdraft")
+    public ResponseEntity<ParKhzcmxDTO> getDraftByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id) {
+        ParKhzcmx domain = new ParKhzcmx();
+        domain.setParassesstemplateid(parassesstemplate_id);
+        return ResponseEntity.status(HttpStatus.OK).body(parkhzcmxMapping.toDto(parkhzcmxService.getDraft(domain)));
+    }
+
+    @PreAuthorize("hasPermission(this.parkhzcmxService.get(#parkhzcmx_id),'ehr-ParKhzcmx-Remove')")
+    @ApiOperation(value = "根据考核模板删除考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板删除考核模板明细")
+	@RequestMapping(method = RequestMethod.DELETE, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/{parkhzcmx_id}")
+    @Transactional
+    public ResponseEntity<Boolean> removeByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @PathVariable("parkhzcmx_id") String parkhzcmx_id) {
+		return ResponseEntity.status(HttpStatus.OK).body(parkhzcmxService.remove(parkhzcmx_id));
+    }
+
+    @PreAuthorize("hasPermission(this.parkhzcmxService.getParkhzcmxByIds(#ids),'ehr-ParKhzcmx-Remove')")
+    @ApiOperation(value = "根据考核模板批量删除考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板批量删除考核模板明细")
+	@RequestMapping(method = RequestMethod.DELETE, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/batch")
+    public ResponseEntity<Boolean> removeBatchByParAssessTemplate(@RequestBody List<String> ids) {
+        parkhzcmxService.removeBatch(ids);
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @ApiOperation(value = "根据考核模板检查考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板检查考核模板明细")
+	@RequestMapping(method = RequestMethod.POST, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/checkkey")
+    public ResponseEntity<Boolean> checkKeyByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @RequestBody ParKhzcmxDTO parkhzcmxdto) {
+        return  ResponseEntity.status(HttpStatus.OK).body(parkhzcmxService.checkKey(parkhzcmxMapping.toDomain(parkhzcmxdto)));
+    }
+
+    @PreAuthorize("hasPermission(this.parkhzcmxService.get(#parkhzcmx_id),'ehr-ParKhzcmx-Update')")
+    @ApiOperation(value = "根据考核模板更新考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板更新考核模板明细")
+	@RequestMapping(method = RequestMethod.PUT, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/{parkhzcmx_id}")
+    @Transactional
+    public ResponseEntity<ParKhzcmxDTO> updateByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @PathVariable("parkhzcmx_id") String parkhzcmx_id, @RequestBody ParKhzcmxDTO parkhzcmxdto) {
+        ParKhzcmx domain = parkhzcmxMapping.toDomain(parkhzcmxdto);
+        domain.setParassesstemplateid(parassesstemplate_id);
+        domain.setParkhzcmxid(parkhzcmx_id);
+		parkhzcmxService.update(domain);
+        ParKhzcmxDTO dto = parkhzcmxMapping.toDto(domain);
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @PreAuthorize("hasPermission(this.parkhzcmxService.getParkhzcmxByEntities(this.parkhzcmxMapping.toDomain(#parkhzcmxdtos)),'ehr-ParKhzcmx-Update')")
+    @ApiOperation(value = "根据考核模板批量更新考核模板明细", tags = {"考核模板明细" },  notes = "根据考核模板批量更新考核模板明细")
+	@RequestMapping(method = RequestMethod.PUT, value = "/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/batch")
+    public ResponseEntity<Boolean> updateBatchByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @RequestBody List<ParKhzcmxDTO> parkhzcmxdtos) {
+        List<ParKhzcmx> domainlist=parkhzcmxMapping.toDomain(parkhzcmxdtos);
+        for(ParKhzcmx domain:domainlist){
+            domain.setParassesstemplateid(parassesstemplate_id);
+        }
+        parkhzcmxService.updateBatch(domainlist);
+        return  ResponseEntity.status(HttpStatus.OK).body(true);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_SUPERADMIN','ehr-ParKhzcmx-Default-all')")
+	@ApiOperation(value = "根据考核模板获取DEFAULT", tags = {"考核模板明细" } ,notes = "根据考核模板获取DEFAULT")
+    @RequestMapping(method= RequestMethod.GET , value="/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/fetchdefault")
+	public ResponseEntity<List<ParKhzcmxDTO>> fetchParKhzcmxDefaultByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id,ParKhzcmxSearchContext context) {
+        context.setN_parassesstemplateid_eq(parassesstemplate_id);
+        Page<ParKhzcmx> domains = parkhzcmxService.searchDefault(context) ;
+        List<ParKhzcmxDTO> list = parkhzcmxMapping.toDto(domains.getContent());
+	    return ResponseEntity.status(HttpStatus.OK)
+                .header("x-page", String.valueOf(context.getPageable().getPageNumber()))
+                .header("x-per-page", String.valueOf(context.getPageable().getPageSize()))
+                .header("x-total", String.valueOf(domains.getTotalElements()))
+                .body(list);
+	}
+
+    @PreAuthorize("hasAnyAuthority('ROLE_SUPERADMIN','ehr-ParKhzcmx-Default-all')")
+	@ApiOperation(value = "根据考核模板查询DEFAULT", tags = {"考核模板明细" } ,notes = "根据考核模板查询DEFAULT")
+    @RequestMapping(method= RequestMethod.POST , value="/parassesstemplates/{parassesstemplate_id}/parkhzcmxes/searchdefault")
+	public ResponseEntity<Page<ParKhzcmxDTO>> searchParKhzcmxDefaultByParAssessTemplate(@PathVariable("parassesstemplate_id") String parassesstemplate_id, @RequestBody ParKhzcmxSearchContext context) {
+        context.setN_parassesstemplateid_eq(parassesstemplate_id);
+        Page<ParKhzcmx> domains = parkhzcmxService.searchDefault(context) ;
+	    return ResponseEntity.status(HttpStatus.OK)
+                .body(new PageImpl(parkhzcmxMapping.toDto(domains.getContent()), context.getPageable(), domains.getTotalElements()));
+	}
+    @PreAuthorize("hasPermission(this.parkhzcmxMapping.toDomain(#parkhzcmxdto),'ehr-ParKhzcmx-Create')")
     @ApiOperation(value = "根据考核方案建立考核模板明细", tags = {"考核模板明细" },  notes = "根据考核方案建立考核模板明细")
 	@RequestMapping(method = RequestMethod.POST, value = "/parjxkhjcszs/{parjxkhjcsz_id}/parkhzcmxes")
     @Transactional
